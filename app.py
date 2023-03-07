1e69a904649164fdc7598585d9ca545d84d3933d
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2023-03-07 09:42:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,22 @@ print("Válassz az alábbi opciók közül:\n\t(1) Családtörténet keresése\n
 valasztas = 0
 
 while valasztas not in range(1,3):
-    valasztas = int(input("\n Melyiket választod?: "))
-print(valasztas)
-
+        valasztas = int(input("Melyiket választod?: "))
+        
 
 if valasztas == 1:
-    print("Családtörténet keresése")
+    print("\n\n    Családtörténet keresése ---\n")
 
     csaladnev = ''
 
     while True:
-        csaladnev = input("  Családnév:")
+        csaladnev = input(" Családnév:")
 
         if csaladnev:
             break
 
 if valasztas == 2:
-    print("Személy keresése")
+    print("\n\n    --- Személy keresése ---\n")
 
 
     vezeteknev = ''
@@ -28,16 +27,16 @@ if valasztas == 2:
     szuletesiEv = ''
 
     while True:
-        vezeteknev = input("  Vezetéknév: (kötelező)")
+        vezeteknev = input(" Vezetéknév: (kötelező)")
 
         if vezeteknev:
             break
 
-    keresztnev = input("  Keresztnév: (nem kötelező)")
+    keresztnev = input(" Keresztnév: (nem kötelező)")
 
-    szuletesiHely = input("  Születési hely: (nem kötelező)")
+    szuletesiHely = input(" Születési hely: (nem kötelező)")
 
-    szuletesiEv = input("  Születési év: (nem kötelező)")
+    szuletesiEv = input(" Születési év: (nem kötelező)")
     
 
     if keresztnev == "":
@@ -50,15 +49,3 @@ if valasztas == 2:
         szuletesiEv = "-"
 
     print(vezeteknev, keresztnev, szuletesiHely, szuletesiEv)
-
-
-    
-
-
-
-
-
-
-
-
-
