a7555b1b40eff851406726f96e50991b644dd327
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2023-03-07 09:35:18 +0100

    alap bekérések

--- app.py
+++ app.py
@@ -1,23 +1,57 @@
+print("Válassz az alábbi opciók közül:\n\t(1) Családtörténet keresése\n\t(2) Személy keresése\n")
 
-print("Válassz az alábbi opciók közül:\n\t (1) Családtörténet keresése\n\t(2) Személy keresése")
-valasztas = ""
-while True:
-    valasztas = input("\n Melyiket választod?: ")
+valasztas = 0
 
-    if valasztas:
-        break
-
+while valasztas not in range(1,3):
+    valasztas = int(input("\n Melyiket választod?: "))
+print(valasztas)
 
 
-csaladnev = ''
+if valasztas == 1:
+    print("Családtörténet keresése")
+
+    csaladnev = ''
+
+    while True:
+        csaladnev = input("  Családnév:")
+
+        if csaladnev:
+            break
+
+if valasztas == 2:
+    print("Személy keresése")
 
 
+    vezeteknev = ''
+    keresztnev = ''
+    szuletesiHely = ''
+    szuletesiEv = ''
 
-while True:
-    vezeteknev = input("Családnév: (kötelező) ")
+    while True:
+        vezeteknev = input("  Vezetéknév: (kötelező)")
 
-    if vezeteknev:
-        break
+        if vezeteknev:
+            break
+
+    keresztnev = input("  Keresztnév: (nem kötelező)")
+
+    szuletesiHely = input("  Születési hely: (nem kötelező)")
+
+    szuletesiEv = input("  Születési év: (nem kötelező)")
+    
+
+    if keresztnev == "":
+        keresztnev = "-"
+    
+    if szuletesiHely == "":
+        szuletesiHely = "-"
+    
+    if szuletesiEv == "":
+        szuletesiEv = "-"
+
+    print(vezeteknev, keresztnev, szuletesiHely, szuletesiEv)
+
+
     
 
 
@@ -28,33 +62,3 @@ while True:
 
 
 
-vezeteknev = ''
-keresztnev = ''
-szuletesiHely = ''
-szuletesiEv = ''
-
-
-
-
-
-
-
-    
-keresztnev = input("Keresztnév: ")
-
-szuletesiHely = input("Születési hely: ")
-
-szuletesiEv = input("Születési év: ")
-
-if keresztnev == "":
-    keresztnev = "-"
-    
-if szuletesiHely == "":
-    szuletesiHely = "-"
-    
-if szuletesiEv == "":
-    szuletesiEv = "-"
-
-
-
-print(vezeteknev, keresztnev, szuletesiHely, szuletesiEv)
