7ffb682d6851e92ec6bcfc1382ca20e01086f2e5
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2023-03-07 08:53:25 +0100

    input elágazás

--- app.py
+++ app.py
@@ -3,17 +3,30 @@ keresztnev = ''
 szuletesiHely = ''
 szuletesiEv = ''
 
+print("\t\tCsaládfa keresése\n")
+
 while True:
-    vezeteknev = input("Családnév: (kötelező) ")
+    vezeteknev = input("Vezetéknév: (kötelező) ")
 
     if vezeteknev:
         break
     
+    
 keresztnev = input("Keresztnév: ")
+
 szuletesiHely = input("Születési hely: ")
+
 szuletesiEv = input("Születési év: ")
 
+if keresztnev == "":
+    keresztnev = "-"
+    
+if szuletesiHely == "":
+    szuletesiHely = "-"
+    
+if szuletesiEv == "":
+    szuletesiEv = "-"
 
 
 
-print(vezeteknev, keresztnev, szuletesiHely, szuletesiev)
+print(vezeteknev, keresztnev, szuletesiHely, szuletesiEv)
