5fcc7fd3fec9f25acda00ac9a8289617ddeae26f
szalkaiszaboadam <bera.marton@diak.szbi-pg.hu>
2023-03-16 15:59:20 +0100

    Bővítés

--- csaladfa.sql
+++ csaladfa.sql
@@ -6,6 +6,8 @@ INSERT INTO person(lastname, firstname, gender, job, placeofbirth, born, placeof
 INSERT INTO person(lastname, firstname, gender, job, placeofbirth, born, placeofdeath, died, causeofdeath) VALUES('Joó', 'Mariann', 'nő', 'üveges', 'Eger', '1989-10-20', NULL, NULL, NULL);
 INSERT INTO person(lastname, firstname, gender, job, placeofbirth, born, placeofdeath, died, causeofdeath) VALUES('Presutto', 'Ivett', 'nő', 'ingatlanos', 'Kiskunfélegyháza', '2006-07-25', NULL, NULL, NULL);
 INSERT INTO person(lastname, firstname, gender, job, placeofbirth, born, placeofdeath, died, causeofdeath) VALUES('Eleben', 'Elemér', 'férfi', 'asztalos', 'Kecskemét', '1972-09-01', NULL, NULL, NULL);
+INSERT INTO person(lastname, firstname, gender, job, placeofbirth, born, placeofdeath, died, causeofdeath) VALUES('Nagy', 'Pista', 'férfi', 'ács', 'Budapest', '1976-03-06', 'Budapest', '2020-11-03', 'természetes');
+INSERT INTO person(lastname, firstname, gender, job, placeofbirth, born, placeofdeath, died, causeofdeath) VALUES('Rácz', 'Dorina', 'nő', 'tanár', 'Bugac', '1974-10-24', NULL, NULL, NULL);
 
 DROP TABLE IF EXISTS parent;
 CREATE TABLE parent(parent_id INTEGER PRIMARY KEY AUTOINCREMENT, person_id INTEGER, parent_person_id INTEGER, relationship TEXT, FOREIGN KEY (person_id) REFERENCES person(person_id), FOREIGN KEY (parent_person_id) REFERENCES person(person_id));
@@ -14,13 +16,15 @@ INSERT INTO parent(person_id, parent_person_id, relationship) VALUES(5, 1, 'apa'
 INSERT INTO parent(person_id, parent_person_id, relationship) VALUES(1, 2, 'apa');
 INSERT INTO parent(person_id, parent_person_id, relationship) VALUES(1, 3, 'anya');
 INSERT INTO parent(person_id, parent_person_id, relationship) VALUES(5, 4, 'anya');
+INSERT INTO parent(person_id, parent_person_id, relationship) VALUES(2, 6, 'apa');
+INSERT INTO parent(person_id, parent_person_id, relationship) VALUES(2, 7, 'anya');
 
 DROP TABLE IF EXISTS marriage;
 CREATE TABLE marriage(marriage_id INTEGER PRIMARY KEY AUTOINCREMENT, datee DATE, place TEXT);
 
 INSERT INTO marriage(datee, place) VALUES(2021-01-03, 'Budapest');
 INSERT INTO marriage(datee, place) VALUES(2018-03-13, 'Csongrád');
-
+INSERT INTO marriage(datee, place) VALUES(2000-04-14, 'Saint-Germain');
 
 DROP TABLE IF EXISTS marriagePerson;
 CREATE TABLE marriagePerson(marriagePerson_id INTEGER PRIMARY KEY AUTOINCREMENT, marriage_id INTEGER, person_id INTEGER, FOREIGN KEY (marriage_id) REFERENCES marriage(marriage_id), FOREIGN KEY (person_id) REFERENCES person(person_id));
@@ -29,4 +33,6 @@ INSERT INTO marriagePerson(marriage_id, person_id) VALUES(1, 2);
 INSERT INTO marriagePerson(marriage_id, person_id) VALUES(1, 3);
 INSERT INTO marriagePerson(marriage_id, person_id) VALUES(2, 1);
 INSERT INTO marriagePerson(marriage_id, person_id) VALUES(2, 4);
+INSERT INTO marriagePerson(marriage_id, person_id) VALUES(3, 6);
+INSERT INTO marriagePerson(marriage_id, person_id) VALUES(3, 7);
 
